a7c667d1ecc6fbc272c5a09e9fec28decb1ff41b
FileNotFound <warner.m22@gmail.com>
2016-02-14 17:08:25 -0600

    Wired up and working Set Temperature

--- NestAPI/Controllers/NestController.cs
+++ NestAPI/Controllers/NestController.cs
@@ -4,29 +4,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
+using System.Web.Http; 
 
 namespace NestAPI.Controllers
 {
     public class NestController : BaseController
     {
-        [HttpGet]
-        public dynamic SetTemperature(int temp)
+        //[HttpGet]
+        //public dynamic SetTemperature(int temp)
+        //{
+        //    try
+        //    {
+        //        var nest = new Nest();
+        //        var thermostats = nest.devices.thermostats;
+        //        var thermostat = thermostats.FirstOrDefault();
+        //        thermostat.target_temperature_f = temp;
+
+        //        return true;
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        return false;
+        //    }
+        //}
+        [HttpPost]
+        public string SetTemperature(dynamic slots)
         {
             try
             {
+                                
                 var nest = new Nest();
                 var thermostats = nest.devices.thermostats;
                 var thermostat = thermostats.FirstOrDefault();
-                thermostat.target_temperature_f = temp;
+                thermostat.target_temperature_f = slots.Temperature.value;
 
-                return true;
+                //thermostat.target_temperature_f = float.Parse(((string)slots.Temperature.value).Replace("\"", ""));
+
+                return string.Format("Temperature has been changed to {0}.", thermostat.target_temperature_f);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                return false;
+                return string.Format("Error occured. Unable to change the temperature. Error message... {0}", ex.Message);
             }
         }
-
     }
 }
