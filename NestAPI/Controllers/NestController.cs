e5c45cbebd86db9f40348e89dfe6f3ab448f0efc
FileNotFound <warner.m22@gmail.com>
2016-02-15 20:23:07 -0600

    Added metheds to get temperature, changed entity to use windows authentication

--- NestAPI/Controllers/NestController.cs
+++ NestAPI/Controllers/NestController.cs
@@ -10,35 +10,16 @@ namespace NestAPI.Controllers
 {
     public class NestController : BaseController
     {
-        //[HttpGet]
-        //public dynamic SetTemperature(int temp)
-        //{
-        //    try
-        //    {
-        //        var nest = new Nest();
-        //        var thermostats = nest.devices.thermostats;
-        //        var thermostat = thermostats.FirstOrDefault();
-        //        thermostat.target_temperature_f = temp;
-
-        //        return true;
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        return false;
-        //    }
-        //}
         [HttpPost]
         public string SetTemperature(dynamic slots)
         {
             try
             {
-                                
+
                 var nest = new Nest();
                 var thermostats = nest.devices.thermostats;
                 var thermostat = thermostats.FirstOrDefault();
                 thermostat.target_temperature_f = slots.Temperature.value;
-
-                //thermostat.target_temperature_f = float.Parse(((string)slots.Temperature.value).Replace("\"", ""));
 
                 return string.Format("Temperature has been changed to {0}.", thermostat.target_temperature_f);
             }
@@ -47,5 +28,40 @@ namespace NestAPI.Controllers
                 return string.Format("Error occured. Unable to change the temperature. Error message... {0}", ex.Message);
             }
         }
+
+        [HttpGet]
+        public string GetCurrentTemperature()
+        {
+            try
+            {
+                var nest = new Nest();
+                var thermostats = nest.devices.thermostats;
+                var thermostat = thermostats.FirstOrDefault();
+
+                return string.Format("Current temperature is {0}.", thermostat.ambient_temperature_f);
+            }
+            catch (Exception ex)
+            {
+                return string.Format("Error occured. Unable to read the current temperature. Error message... {0}", ex.Message);
+            }
+        }        
+        
+        [HttpGet]
+        public string GetTargetTemperature()
+        {
+            try
+            {
+                var nest = new Nest();
+                var thermostats = nest.devices.thermostats;
+                var thermostat = thermostats.FirstOrDefault();
+
+                return string.Format("Target temperature is {0}.", thermostat.target_temperature_f);
+            }
+            catch (Exception ex)
+            {
+                return string.Format("Error occured. Unable to read the target temperature. Error message... {0}", ex.Message);
+            }
+        }
+        
     }
 }
